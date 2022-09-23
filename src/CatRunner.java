4c981dd48751d3ee9a5c7d72d0daad13d91452d1
Bonnie <bonniec15@nycstudents.net>
2022-09-23 10:51:42 -0400

    Rectangles

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -1,5 +1,11 @@
 public class CatRunner {
     public static void main (String [] args) {
+        Cat cat1 = new Cat("Fish", 10,20.5);
+        cat1.introduce();
+        cat1.printCatInfo();
 
+        Cat cat2 = new Cat("hehe", 5,15.2);
+        cat2.introduce();
+        cat2.printCatInfo();
     }
 }
